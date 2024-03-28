e43820591b954074b9c6bff9c81952db11cd8311
ViniciusSXavier999 <viniciusxaviersouza11@gmail.com>
2024-03-28 02:59:46 -0300

    Estrutura inicial do projeto

---
+++ src/test/java/br/com/vx/picpay/senior/backend/challenge/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vx.picpay.senior.backend.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
